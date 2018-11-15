d6471f9e33995aaa57d1613400c0dc2c4e90af66
ssong-eu <sangyoon.song@gmail.com>
2018-11-15 15:19:01 +0000

    temporary hold on further dev, focusing on websockets

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_and_belongs_to_many :chatrooms
     has_many :sent_messages, :class_name => 'Message', :foreign_key => 'sender_id', dependent: :destroy
     has_many :received_messages, :class_name => 'Message', :foreign_key => 'receiver_id'
+    validates :usersignkey, presence: true
 end
